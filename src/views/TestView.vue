58ffb70cacfeeca06db539dcd3fb11a9d11a6c77
zhangjianxin <zhjx2000@sina.cn>
2025-05-20 08:36:48 +0800

    修改测试页样式

---
+++ src/views/TestView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <a-button type="primary" @click="test">Click</a-button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const test = () => {
+  console.log('test')
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  padding-top: 60px;
+}
+</style>
